12248e6c580b52b2a25a874c83cd03eaa32bf2bb
tjgersho <travis.g@paradigmmotion.com>
2017-12-13 22:02:56 +0000

    Working on it..

--- kidtrack/src/app/app.component.ts
+++ kidtrack/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HostListener } from '@angular/core';
 import { GlobalService } from './services/global.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +11,20 @@ import { GlobalService } from './services/global.service';
 export class AppComponent {
   title = 'KidTrack';
 
-	constructor(private global: GlobalService){
+	constructor(private global: GlobalService, private router: Router){
 			
 	   global.onWindowChange.subscribe((w)=>{
 		console.log("On window change global observable in index");
 		console.log(w);
             });
-
+           
+         router.events.subscribe((evt) => {
+            if (!(evt instanceof NavigationEnd)) {
+                return;
+             }
+            window.scrollTo(0, 0)
+          });
+		
 	}
 	@HostListener('window:resize', ['$event'])
 	 onResize(event) {
